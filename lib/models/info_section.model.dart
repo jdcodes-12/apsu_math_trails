bc0684ea5f971a604c7e77cda61b2928f8a33c3e
Johnny Dickson <johnnydicksoncodes@gmail.com>
2023-03-09 19:29:15 -0600

    Partially implement InfoSection
    Need to figure out how to handle List<Question> json parse

--- lib/models/info_section.model.dart
+++ lib/models/info_section.model.dart
@@ -1,3 +1,22 @@
 class InfoSection {
-  
+  final String stopId;
+  final String sectionHeader;
+  // final List<Question> questions;
+
+  InfoSection._({
+    required this.stopId,
+    required this.sectionHeader,
+  });
+
+  factory InfoSection.fromJson(Map<String, dynamic> json) {
+    return InfoSection._(
+      stopId: json["stop_id"],
+      sectionHeader: json["section_header"]
+    ); 
+  }
+
+  Map<String, dynamic> toJson() => {
+    "stop_id": stopId,
+    "section_header": sectionHeader,
+  };
 }
