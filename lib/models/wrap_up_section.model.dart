7502c1ab61506241761ad22f58238a2c65a2fe01
Johnny Dickson <johnnydicksoncodes@gmail.com>
2023-03-09 19:35:41 -0600

    Implement WrapUpSection model class

--- lib/models/wrap_up_section.model.dart
+++ lib/models/wrap_up_section.model.dart
@@ -1,3 +1,25 @@
 class WrapUpSection {
-  
+  final String stopId;
+  final String sectionHeader;
+  final String sectionBody;
+
+  WrapUpSection._({
+    required this.stopId,
+    required this.sectionHeader,
+    required this.sectionBody,
+  });
+
+  factory WrapUpSection.fromJson(Map<String, dynamic> json) {
+    return WrapUpSection._(
+      stopId: json["stop_id"],
+      sectionHeader: json["section_header"],
+      sectionBody: json["section_body"],
+    ); 
+  }
+
+  Map<String, dynamic> toJson() => {
+    "stop_id": stopId,
+    "section_header": sectionHeader,
+    "section_body": sectionBody,
+  };
 }
