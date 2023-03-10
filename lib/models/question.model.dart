0284678c6a08ad89f486ba9c7dabc8c44cfa2f35
Johnny Dickson <johnnydicksoncodes@gmail.com>
2023-03-09 19:40:32 -0600

    Partially implement Question model class.
    Need to handle parsing nested JSON for answer choices.

--- lib/models/question.model.dart
+++ lib/models/question.model.dart
@@ -1,3 +1,20 @@
-class ObjectiveSection {
-  
+class Question {
+  final String questionId;
+  final String prompt;
+  final String correctAnswerLabel;
+  // final List<Answer> answerChoices;
+
+  Question._({
+    required this.questionId,
+    required this.prompt,
+    required this.correctAnswerLabel,
+  });
+
+  factory Question.fromJson(json) {
+    return Question._(
+      questionId: json["question_id"],
+      prompt: json["prompt"],
+      correctAnswerLabel: json["correct_answer_label"],
+    );
+  } 
 }
