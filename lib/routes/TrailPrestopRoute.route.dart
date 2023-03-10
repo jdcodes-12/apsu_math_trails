108e8cc422bdb5e51014b1cbcebf7f899d8073cb
Johnny Dickson <johnnydicksoncodes@gmail.com>
2023-03-10 15:43:37 -0600

    Scaffold routes
    Stubbed routes for testing the routing. UIs for routes still need to be implemented.

---
+++ lib/routes/TrailPrestopRoute.route.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TrailPrestopRoute extends StatelessWidget {
+  const TrailPrestopRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
