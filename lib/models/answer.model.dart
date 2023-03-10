b0c4a88c0687f951a2bca041a1248e92af8f419d
Johnny Dickson <johnnydicksoncodes@gmail.com>
2023-03-09 19:27:46 -0600

    Implement Answer model class

--- lib/models/answer.model.dart
+++ lib/models/answer.model.dart
@@ -1,3 +1,21 @@
 class Answer {
-  
+  final String response;
+  final String label;
+
+  Answer._({
+    required this.response,
+    required this.label,
+  });
+
+  factory Answer.fromJson(Map<String, dynamic> json) {
+    return Answer._(
+      response: json["response"],
+      label: json["label"],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    "response": response,
+    "label": label,
+  };
 }
