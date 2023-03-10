f7e4ee368beba33974da93c538dfd18828c657f8
Johnny Dickson <johnnydicksoncodes@gmail.com>
2023-03-09 19:33:59 -0600

    Partially implement ObjectiveSection model class
    Need to handle parsing nested JSON for questions field

--- lib/models/objective_section.model.dart
+++ lib/models/objective_section.model.dart
@@ -1,3 +1,22 @@
 class ObjectiveSection {
-  
+    final String stopId;
+    final String sectionHeader;
+    // final List<Question> questions;
+
+    ObjectiveSection._({
+      required this.stopId,
+      required this.sectionHeader,
+    });
+
+    factory ObjectiveSection.fromJson(Map<String, dynamic> json) {
+      return ObjectiveSection._(
+        stopId: json["stop_id"],
+        sectionHeader: json["section_header"]
+      ); 
+    }
+
+    Map<String, dynamic> toJson() => {
+      "stop_id": stopId,
+      "section_header": sectionHeader,
+    };
 }
