69e26693917bcee2c1cab947dbcdb83b52f1fd1e
Johnny Dickson <johnnydicksoncodes@gmail.com>
2023-03-10 20:08:41 -0600

    Implement nested parsing logic for Trail model

--- lib/models/trail.model.dart
+++ lib/models/trail.model.dart
@@ -1,40 +1,48 @@
+import 'package:math_trails/models/stop.model.dart';
+import 'package:math_trails/utils/JsonParser.util.dart';
+
 class Trail {
   final int trailId;
-  final String trailName_Full;
-  final String trailName_Short;
-  final String trailDescription_Full;
-  final String trailDescription_Short;
+  final String trailNameFull;
+  final String trailNameShort;
+  final String trailDescriptionFull;
+  final String trailDescriptionShort;
   final double durationInMinutes;
-  // final List<Stop> stops;
+  final List<Stop> stops;
 
   Trail._({
     required this.trailId,
-    required this.trailName_Full,
-    required this.trailName_Short,
-    required this.trailDescription_Full,
-    required this.trailDescription_Short,
+    required this.trailNameFull,
+    required this.trailNameShort,
+    required this.trailDescriptionFull,
+    required this.trailDescriptionShort,
     required this.durationInMinutes,
-    // required this.stops,
+    required this.stops,
   }); 
 
   factory Trail.fromJson(Map<String, dynamic> json) {
+    List<dynamic> stopsJson = json['stops'];
+    List<Stop> stops = 
+      stopsJson.map((stop) => Stop.fromJson(stop)).toList();
+
     return Trail._(
       trailId: json['trail_id'],
-      trailName_Full: json['trail_name_full'],
-      trailName_Short: json['trail_name_short'],
-      trailDescription_Full: json['trail_description_full'],
-      trailDescription_Short: json['trail_description_short'],
+      trailNameFull: json['trail_name_full'],
+      trailNameShort: json['trail_name_short'],
+      trailDescriptionFull: json['trail_description_full'],
+      trailDescriptionShort: json['trail_description_short'],
       durationInMinutes: json['duration_in_minutes'],
-      // Stops
+      stops: stops,
     );
   }
-
+  
   Map<String, dynamic> toJson() => {
     "trail_id": trailId,
-    "trail_name_full": trailName_Full,
-    "trail_name_short": trailDescription_Short,
-    "trail_description_full": trailDescription_Full,
-    "trail_description_short": trailDescription_Short,
+    "trail_name_full": trailNameFull,
+    "trail_name_short": trailDescriptionShort,
+    "trail_description_full": trailDescriptionFull,
+    "trail_description_short": trailDescriptionShort,
     "duration_in_minutes": durationInMinutes,
+    "stops": stops.toString(),
   };
 }
