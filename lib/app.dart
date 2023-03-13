755824edf1d7fa25eb1d3c1456be2b8acaf3cb55
Johnny Dickson <johnnydicksoncodes@gmail.com>
2023-03-12 23:31:15 -0500

    Refactor file names.
    Separated MyApp from main.dart to include only setup logic in main.

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:math_trails/routes.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Math Trails | Firebase App',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      initialRoute: RouteGenerator.homeRoute,
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
