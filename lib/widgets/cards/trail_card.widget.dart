b1585f3bddaa6f9f409a59d79a9da7a6a67a789e
Johnny Dickson <johnnydicksoncodes@gmail.com>
2023-03-13 00:25:13 -0500

    Scaffold TrailCard
    Created TrailCard widget to increase readability in TrailsList. Need to refactor TrailCard to be more generic, being built based on arguments.

--- lib/widgets/cards/trail_card.widget.dart
+++ lib/widgets/cards/trail_card.widget.dart
@@ -1,17 +1,36 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
-
-// TODO: Implement routing to Trail X's TrailCardSpotlight
-//    - Takes a Trail
+import 'package:math_trails/routes.dart';
 
 class TrailCard extends StatelessWidget {
   const TrailCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      width: 300,
-      child: Card(elevation: 16.0, child: Column()),
+    return InkWell(
+      onDoubleTap: () {
+        log('pressed');
+        Navigator.pushNamed(context, RouteGenerator.trailPrestopRoute,
+            arguments: {
+              "trail_id": 1,
+              "trail_name": "Trail 1 - Name",
+            });
+        log('Moving to TrailPrestopRoute...');
+      },
+      child: Card(
+        child: Column(
+          children: const [
+            Text(
+              "Trail X",
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w600,
+                color: Colors.deepPurpleAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
